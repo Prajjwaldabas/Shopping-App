ebbec5d11acee9371ab1afece900aed3939dd9b6
Prajjwaldabas <Prajjwalchaudhary29898@gmail.com>
2023-04-15 22:50:45 +0530

    new commit in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -31,13 +31,13 @@ app.use(cors({
 app.use(express.json());
 
 
-app.use(express.static(path.join(__dirname, 'Shoppingapp/frontend/e-commerce-app-frontend/public')));
+app.use(express.static(path.join(__dirname, 'ShoppingAPP/frontend/e-commerce-app-frontend/public')));
 
 // app.use('/users', usersRouter);
 app.use('/',require('./routes/index'))
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'Shoppingapp/frontend/e-commerce-app-frontend/public/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, 'ShoppingAPP/frontend/e-commerce-app-frontend/public/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
