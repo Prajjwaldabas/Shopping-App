5e0aa3471189d8a5c02f41177479a9c920a4c3ec
Prajjwaldabas <Prajjwalchaudhary29898@gmail.com>
2023-04-15 22:19:14 +0530

    new commit

--- Backend/server.js
+++ Backend/server.js
@@ -33,9 +33,13 @@ app.use(express.json());
 // app.use('/users', usersRouter);
 app.use('/',require('./routes/index'))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 
 
