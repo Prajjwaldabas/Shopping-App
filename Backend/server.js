fe778637944f460656bb29b234abb0c44c3f1f42
Prajjwaldabas <Prajjwalchaudhary29898@gmail.com>
2023-04-15 15:09:36 +0530

    new commit in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,29 @@
-const express = require('express');
+const express= require('express');
 const app = express();
-const dotenv = require('dotenv');
+const dotenv= require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const path = require('path')
 const db = require('./Database/Mongoose');
 
-dotenv.config({ path: 'config.env' })
+dotenv.config({path: 'config.env'})
 const PORT = process.env.PORT || 8000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
-
+//
 app.use(cors());
-
-const allowedOrigins = ['http://localhost:3000', 'https://e-commerce-app-q0tc.onrender.com'];
 app.use(cors({
-  origin: allowedOrigins
-}));
-
+    origin: ['http://localhost:3000','https://e-commerce-server-hhpk.onrender.com','https://e-commerce-app-q0tc.onrender.com']
+  }));
+//  
 app.use(express.json());
 
-app.use('/', require('./routes/index'))
+// app.use('/users', usersRouter);
+app.use('/',require('./routes/index'))
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`)
+
+app.listen (PORT,()=>{
+    console.log(`Server is running on port: ${PORT}`)
 })
