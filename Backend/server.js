37809aa283d0064c8113368890a96598331ce267
Prajjwaldabas <Prajjwalchaudhary29898@gmail.com>
2023-04-15 14:16:30 +0530

    server.js cors change

--- Backend/server.js
+++ Backend/server.js
@@ -1,29 +1,25 @@
-const express= require('express');
+const express = require('express');
 const app = express();
-const dotenv= require('dotenv');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const path = require('path')
 const db = require('./Database/Mongoose');
 
-dotenv.config({path: 'config.env'})
+dotenv.config({ path: 'config.env' })
 const PORT = process.env.PORT || 8000;
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//
-app.use(cors());
 app.use(cors({
-    origin: ['http://localhost:3000','https://e-commerce-server-hhpk.onrender.com']
-  }));
-//  
+  origin: ['http://localhost:3000', 'https://e-commerce-server-hhpk.onrender.com', 'https://e-commerce-app-q0tc.onrender.com']
+}));
+
 app.use(express.json());
 
-// app.use('/users', usersRouter);
-app.use('/',require('./routes/index'))
+app.use('/', require('./routes/index'))
 
-
-app.listen (PORT,()=>{
-    console.log(`Server is running on port: ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`)
 })
